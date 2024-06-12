21c11881d5398125f546fbaa2c286e7a71f44cca
mkulker <mkulker@umass.edu>
2024-06-12 10:18:21 -0400

    finished projet and test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import $ from 'jquery'; // Import jQuery
+var category = ''
+const xLogo = require('./xLogo.webp');
 
 function App() {
+const [quote, setQuote] = useState({});
+const tweetUrl = `https://twitter.com/intent/tweet?hashtags=quotes&text=${encodeURIComponent(`"${quote.quote}" - ${quote.author}`)}`;
+const handleButton = ()=>{
+    $.ajax({
+      method: 'GET',
+      url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
+      headers: { 'X-Api-Key': 'Fu/R7pNv1msFHpLzH3E1mg==umITEdtdtZp3JQlH'},
+      contentType: 'application/json',
+      success: function(result) {
+        setQuote(result[0]);
+      },
+      error: function ajaxError(jqXHR) {
+          console.error('Error: ', jqXHR.responseText);
+      }
+    });
+  };
+
+  useEffect(()=>{
+    handleButton()
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section id ='quote-box'>
+            <h1 id='text'>"{quote.quote}</h1>
+            <h3 id='author'>-{quote.author}</h3>
+            <a id='tweet-quote' href={tweetUrl}> <img src={xLogo} alt = 'X tweet logo'></img></a><button id='new-quote' onClick={handleButton}> New Quote</button>
+        </section>
       </header>
+
     </div>
   );
 }
